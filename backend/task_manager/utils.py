0407675e9f0c863c2521f8f4cb53549f4259b071
Nazmus Saif <nazmussaif.cse@gmail.com>
2025-04-11 13:10:11 +0600

    Refactor email sending functions to use HTML templates for improved formatting

--- backend/task_manager/utils.py
+++ backend/task_manager/utils.py
@@ -3,41 +3,40 @@ from channels.layers import get_channel_layer
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import Notifications
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 def send_user_created_email(user, password):
     subject = "Your User Account Has Been Created"
-    message = f"""
-    Hello {user.name},
 
-    Your user account has been successfully created.
+    html_message = render_to_string("account_creation.html", {
+                                    "user": user, "password": password})
+    plain_message = strip_tags(html_message)
 
-    Here are your login credentials:
-    Email: {user.email}
-    Password: {password}  (Please do not share this password with anyone else)
-
-    You can log in to your account using the credentials above.
-
-    Best regards,
-    The Team
-    """
     from_email = settings.DEFAULT_FROM_EMAIL
     to_email = [user.email]
 
     try:
-        send_mail(subject, message, from_email, to_email)
+        send_mail(subject, plain_message, from_email,
+                  to_email, html_message=html_message)
     except Exception as e:
         print(f"Failed to send email: {str(e)}")
 
 
 def send_task_assigned_email(user, task):
-    subject = f"New Task Assigned: {task.title}"
-    message = f"Hello {user.name},\n\nYou have been assigned a new task: {task.title}.\n\nDescription:\n{task.description}\n\nPriority: {task.priority}\n\nDeadline: {task.deadline}"
+    subject = f"New Task Assigned"
+
+    html_message = render_to_string(
+        "task_assigned.html", {"user": user, "task": task})
+    plain_message = strip_tags(html_message)
+
     from_email = settings.DEFAULT_FROM_EMAIL
     to_email = [user.email]
 
     try:
-        send_mail(subject, message, from_email, to_email)
+        send_mail(subject, plain_message, from_email,
+                  to_email, html_message=html_message)
     except Exception as e:
         print(f"Failed to send email: {str(e)}")
 
